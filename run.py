95a670c2ade12a4c593adba55195a6e8466fdf8f
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-01-25 23:01:47 +0000

    Get time from user, validate and convert to seconds

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import time
+import datetime 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,24 +22,24 @@ def typed(string):
     """
     for i in string:
         print(i, end="", flush=True)
-        time.sleep(0.05)
+        time.sleep(0.03)
 
 
-def user_age():
+def get_user_age():
     """
     Get user age data
     Run a while loop to get valid data from the user
     """
     while True:
         typed("Please enter your age\n")
-        age = input("Age: ") # Add a newline character before deployment
+        user_age = input("Age: ") # Add a newline character before deployment
 
-        if validate_age(age):
-            typed(f"You entered: {age}")
+        if validate_age(user_age):
+            typed(f"You entered: {user_age}")
             print("\n")
             break
     
-    return age
+    return user_age
     
 def validate_age(data):
     """
@@ -47,8 +48,8 @@ def validate_age(data):
     Raises TypeError if age falls outside the range 10-100
     """
     try: 
-        [int(num) for num in data]
-        if int(data) < 10 | int(data) > 100:
+        a = int(data)
+        if a < 10 or a > 100:
             raise TypeError
     except ValueError:
         typed("Invalid age, please enter a whole number between 10 and 100")
@@ -62,20 +63,21 @@ def validate_age(data):
     return True
     
 
-def user_gender():
+def get_user_gender():
     """
     Get user gender data
     Run a while loop to get valid data from the user
     """
     while True:
         typed("Please enter your gender: m/f\n")
-        gender = input("Gender: ") # Add a newline character before deployment
+        user_gender = input("Gender: ") # Add a newline character before deployment
 
-        if validate_gender(gender):
-            typed(f"You entered: {gender}")
+        if validate_gender(user_gender):
+            typed(f"You entered: {user_gender}")
+            print("\n")
             break
     
-    return gender
+    return user_gender
 
 
 def validate_gender(data):
@@ -93,16 +95,92 @@ def validate_gender(data):
 
     return True
 
+def get_user_row_time():
+    """
+    Get time for latest row
+    Run a while loop to get valid data from the user
+    Convert inputs into a readable string and and a computable integer
+    """
+    typed("Please enter your latest 2k test time. Minutes, then seconds, then tenths\n")
+    while True:
+        user_row_minutes = input("Minutes: ") # Add a newline character before deployment
+        if validate_minutes(user_row_minutes):
+            user_row_seconds = input("Seconds: ") # Add a newline character before deployment
+            if validate_seconds(user_row_seconds):
+                user_row_tenths = input("Tenths: ") # Add a newline character before deployment
+                user_row_time = f"{user_row_minutes}:{user_row_seconds.zfill(2)}.{user_row_tenths}"
+                if validate_tenths(user_row_tenths):
+                    typed(f"You entered: {user_row_time}")
+                    print("\n")
+                    break
+ 
+    actual_time = (int(user_row_minutes) * 60) + int(user_row_seconds) + (int(user_row_tenths) / 10)
+
+    return actual_time       
+
+def validate_minutes(data):
+    """
+    Inside the try, converts time to integer
+    Raises ValueError if the string can't be converted or if minutes 
+    fall ouside the range 1-59
+    """
+    try:
+        m = int(data)
+        if m < 1 or m > 59:
+            raise ValueError
+    except ValueError:
+        typed("Invalid entry, please enter a minutes value between 1 and 59")
+        print("\n")
+        return False
+    
+    return True
+
+
+def validate_seconds(data):
+    """
+    Inside the try, converts time to integer
+    Raises ValueError if the string can't be converted or if seconds 
+    fall ouside the range 0-59
+    """
+    try:
+        m = int(data)
+        if m < 0 or m > 59:
+            raise ValueError
+    except ValueError:
+        typed("Invalid entry, please enter a seconds value between 0 and 59")
+        print("\n")
+        return False
+    
+    return True
+
+def validate_tenths(data):
+    """
+    Inside the try, converts time to integer
+    Raises ValueError if the string can't be converted or if tenths 
+    fall ouside the range 0-9
+    """
+    try:
+        m = int(data)
+        if m < 0 or m > 9:
+            raise ValueError
+    except ValueError:
+        typed("Invalid entry, please enter a tenths value between 0 and 9")
+        print("\n")
+        return False
+    
+    return True
+
 def main():
     """
     Runs all functions
     """
-    typed("Welcome to Row Assist, your indoor rowing assistant.\n")
-    typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
-    typed("I will calculate your split time and watts and provide you with a ranking.\n")
-    print("\n")
-    age = user_age()
-    gender = user_gender()
-
+    # typed("Welcome to Row Assist, your indoor rowing assistant.\n")
+    # typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
+    # typed("I will calculate your split time and watts and provide you with a ranking.\n")
+    # print("\n")
+    # age = get_user_age()
+    # gender = get_user_gender()
+    row_time = get_user_row_time()
+    print(row_time)
 
 main()
