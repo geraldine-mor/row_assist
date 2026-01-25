625dead9a3558ebe3efbb165f6920dded80339cb
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-01-25 16:31:40 +0000

    Add function to get user's gender and validate. Add main function

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ def user_age():
 
         if validate_age(age):
             typed(f"You entered: {age}")
+            print("\n")
             break
     
     return age
@@ -43,13 +44,14 @@ def validate_age(data):
     """
     Inside the try, converts age to an integer.
     Raises ValueError if string cannot be converted into int
+    Raises TypeError if age falls outside the range 10-100
     """
     try: 
         [int(num) for num in data]
         if int(data) < 10 | int(data) > 100:
             raise TypeError
     except ValueError:
-        typed(f"Invalid age, please enter a whole number between 10 and 100")
+        typed("Invalid age, please enter a whole number between 10 and 100")
         print("\n")
         return False
     except TypeError:
@@ -60,7 +62,47 @@ def validate_age(data):
     return True
     
 
-typed("Welcome to Row Assist, your indoor rowing assistant.\n")
-typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
-typed("I will calculate your split time and watts and provide you with a ranking.\n")
-age = user_age()
+def user_gender():
+    """
+    Get user gender data
+    Run a while loop to get valid data from the user
+    """
+    while True:
+        typed("Please enter your gender: m/f\n")
+        gender = input("Gender: ") # Add a newline character before deployment
+
+        if validate_gender(gender):
+            typed(f"You entered: {gender}")
+            break
+    
+    return gender
+
+
+def validate_gender(data):
+    """
+    Inside the try, checks for m or f character.
+    Raises ValueError if any other character is entered
+    """
+    try: 
+        if data.lower() != "f" and data.lower() != "m":
+            raise ValueError
+    except ValueError:
+        typed("Invalid entry, please enter either m or f")
+        print("\n")
+        return False
+
+    return True
+
+def main():
+    """
+    Runs all functions
+    """
+    typed("Welcome to Row Assist, your indoor rowing assistant.\n")
+    typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
+    typed("I will calculate your split time and watts and provide you with a ranking.\n")
+    print("\n")
+    age = user_age()
+    gender = user_gender()
+
+
+main()
