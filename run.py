3d02b3ff5a356cc3d7f8d541cf77db86fc3012d4
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-01-26 10:11:11 +0000

    Improve time input and validation loops

--- run.py
+++ run.py
@@ -102,44 +102,47 @@ def get_user_row_time():
     Convert inputs into a readable string and and a computable integer
     """
     typed("Please enter your latest 2k test time. Minutes, then seconds, then tenths\n")
+    user_minutes = get_time("minutes")
+    user_seconds = get_time("seconds")
+    user_tenths = get_time_tenths()            
+    user_row_time = f"{user_minutes}:{user_seconds.zfill(2)}.{user_tenths}"
+                
+    typed(f"You entered: {user_row_time}")
+    print("\n")               
+    actual_time = (int(user_minutes) * 60) + int(user_seconds) + (int(user_tenths) / 10)
+    check_wr(actual_time)
+    
+    return actual_time   
+
+
+def get_time(data):
+    """
+    Get time values for latest row
+    Run a while loop to get valid data from the user
+    """
     while True:
-        user_row_minutes = input("Minutes: ") # Add a newline character before deployment
-        if validate_minutes(user_row_minutes):
-            user_row_seconds = input("Seconds: ") # Add a newline character before deployment
-            if validate_seconds(user_row_seconds):
-                user_row_tenths = input("Tenths: ") # Add a newline character before deployment
-                user_row_time = f"{user_row_minutes}:{user_row_seconds.zfill(2)}.{user_row_tenths}"
-                if validate_tenths(user_row_tenths):
-                    typed(f"You entered: {user_row_time}")
-                    print("\n")
-                    break
- 
-    actual_time = (int(user_row_minutes) * 60) + int(user_row_seconds) + (int(user_row_tenths) / 10)
+        user_input = input(f"{data.capitalize()}: ") # Add a newline character before deployment
+        if validate_time(user_input):
+            return user_input
 
-    return actual_time       
 
-def validate_minutes(data):
+def get_time_tenths():
+    """
+    Get tenths value for latest row
+    Run a while loop to get valid data from the user
+    """
+    while True:
+        tenths = input("Tenths: ") # Add a newline character before deployment
+        if validate_tenths(tenths):
+            break
+
+    return tenths
+
+
+def validate_time(data):
     """
     Inside the try, converts time to integer
     Raises ValueError if the string can't be converted or if minutes 
-    fall ouside the range 1-59
-    """
-    try:
-        m = int(data)
-        if m < 1 or m > 59:
-            raise ValueError
-    except ValueError:
-        typed("Invalid entry, please enter a minutes value between 1 and 59")
-        print("\n")
-        return False
-    
-    return True
-
-
-def validate_seconds(data):
-    """
-    Inside the try, converts time to integer
-    Raises ValueError if the string can't be converted or if seconds 
     fall ouside the range 0-59
     """
     try:
@@ -147,11 +150,12 @@ def validate_seconds(data):
         if m < 0 or m > 59:
             raise ValueError
     except ValueError:
-        typed("Invalid entry, please enter a seconds value between 0 and 59")
+        typed("Invalid entry, please enter a minutes value between 0 and 59")
         print("\n")
         return False
     
     return True
+
 
 def validate_tenths(data):
     """
@@ -170,17 +174,24 @@ def validate_tenths(data):
     
     return True
 
+def check_wr(data):
+    if data < 335.8:
+        typed("Greeting Barry Allen!\n")
+        typed("The fastest 2k ever recorded is 5:35.8 and you smashed it!")
+        print("\n")
+
+
 def main():
     """
     Runs all functions
     """
-    # typed("Welcome to Row Assist, your indoor rowing assistant.\n")
-    # typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
-    # typed("I will calculate your split time and watts and provide you with a ranking.\n")
-    # print("\n")
-    # age = get_user_age()
-    # gender = get_user_gender()
+    typed("Welcome to Row Assist, your indoor rowing assistant.\n")
+    typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
+    typed("I will calculate your split time and watts and provide you with a ranking.\n")
+    print("\n")
+    age = get_user_age()
+    gender = get_user_gender()
     row_time = get_user_row_time()
-    print(row_time)
+    
 
 main()
