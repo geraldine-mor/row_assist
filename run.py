c65ab394231b96cd423870c165f9ce55b9d4f968
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-01-26 15:17:39 +0000

    Define function to merge 2 colums into a list of tuples

--- run.py
+++ run.py
@@ -19,8 +19,9 @@ SHEET = GSPREAD_CLIENT.open("row_assist")
 
 def calculate_splits(data):
     """
-    Calculates the time per 500m from the data provided
-    Converts resul into readable time format
+    Calculates the time per 500m from the data provided 
+    Time(s) / (distance / 500)
+    Converts result into readable time format
     """
     distance = 2000
     calc_split = data / (distance / 500)
@@ -33,30 +34,60 @@ def calculate_splits(data):
 
 
 def calculate_watts(data):
+    """
+    Converts split times into watts
+    2.8 / (split(s) / 500)^3
+    Rounds to nearest int as per 
+    """
     watts = round(2.8 / (data / 500)**3)
     typed(f"Your watts generated are: {watts}\n")
 
     return watts
 
-def get_reference(data):
+def get_reference_min_age(data):
+    """
+    Retrieve min age column from google sheets
+    """
     ref_sheet = SHEET.worksheet(f"{data}_2000")
-    ref_data = ref_sheet.get_all_values()
-    print(ref_data)
+    age_min = ref_sheet.col_values(1)
+    
+    return age_min
+
+
+def get_reference_max_age(data):
+    """
+    Retrieve max age column from google sheets
+    """
+    ref_sheet = SHEET.worksheet(f"{data}_2000")
+    age_max = ref_sheet.col_values(2)
+    
+    return age_max
+
+
+def ages_tuple(min, max):
+    """
+    Creates a list of tuples (min, max)
+    """
+    reference_ages = [(x, y) for x, y in zip(min, max)]
+    print(reference_ages)
+    
 
 
 def main():
     """
     Runs all functions
     """
-    typed("Welcome to Row Assist, your indoor rowing assistant.\n")
-    typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
-    typed("I will calculate your split time and watts and provide you with a ranking.")
-    print("\n")
+    # typed("Welcome to Row Assist, your indoor rowing assistant.\n")
+    # typed("You will soon be asked for your gender, age and your latest 2k test time.\n")
+    # typed("I will calculate your split time and watts and provide you with a ranking.")
+    # print("\n")
     # age = data.get_user_age()
     gender = data.get_user_gender()
     # row_time = data.get_user_row_time()
-    # split_time = calculate_splits(row_time)
-    # user_watts = calculate_watts(split_time)
-    get_reference(gender)
+    # user_split = calculate_splits(row_time)
+    # user_watts = calculate_watts(user_split)
+    min_age = get_reference_min_age(gender)
+    max_age = get_reference_max_age(gender)
+    ages_tuple(min_age, max_age)
 
 main()
